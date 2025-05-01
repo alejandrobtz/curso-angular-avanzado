cf0ae5a824555d2a32b3342a06e932d8e5aaaf58
Alejandro <mabtzr@gmail.com>
2025-05-01 12:31:55 -0500

    setting dev, prod environments

--- src/app/domains/shared/services/category.service.ts
+++ src/app/domains/shared/services/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Category } from '@shared/models/category.model';
+import { environment } from '@env/environment';
 
 @Injectable({
 	providedIn: 'root',
@@ -8,9 +9,9 @@ import { Category } from '@shared/models/category.model';
 export class CategoryService {
 	private http = inject(HttpClient);
 
+	readonly API_URL = environment.apiUrl;
+
 	getAll() {
-		return this.http.get<Category[]>(
-			`https://api.escuelajs.co/api/v1/categories`
-		);
+		return this.http.get<Category[]>(`${this.API_URL}/api/v1/categories`);
 	}
 }
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
+import { environment } from '@env/environment';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ProductService {
 	private http = inject(HttpClient);
+	readonly API_URL = environment.apiUrl;
 
 	getProducts(category_id?: string) {
-		const url = new URL(`https://api.escuelajs.co/api/v1/products`);
+		const url = new URL(`${this.API_URL}/api/v1/products`);
 		if (category_id) {
 			url.searchParams.set('categoryId', category_id);
 		}
@@ -17,8 +19,6 @@ export class ProductService {
 	}
 
 	getOne(id: string) {
-		return this.http.get<Product>(
-			`https://api.escuelajs.co/api/v1/products/${id}`
-		);
+		return this.http.get<Product>(`${this.API_URL}/api/v1/products/${id}`);
 	}
 }
